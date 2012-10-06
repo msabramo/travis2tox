99f24014d712024187afb4c63ffd4033bd385467
Marc Abramowitz <marc@marc-abramowitz.com>
2012-10-05 21:38:43 -0700

    Add test for testing that the command-line program works

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,3 +1,6 @@
+import os
+import subprocess
+import sys
 import unittest
 
 try:
@@ -6,6 +9,13 @@ try:
 except ImportError:
     # Python 3
     from io import StringIO
+
+if sys.version_info > (2, ):
+    def b(s):
+        return s.encode('utf-8')
+else:
+    def b(s):
+        return s
 
 from travis2tox import travis2tox
 
@@ -164,3 +174,49 @@ commands =
 """
 
         self.assertEqual(travis2tox(input).getvalue().strip(), expected_output.strip())
+
+
+class CommandTests(unittest.TestCase):
+
+    def setUp(self):
+        os.chdir(os.path.dirname(__file__))
+
+    def travis2tox(self, *args):
+        travis2tox_path = os.path.join(os.path.dirname(sys.executable), "travis2tox")
+        return self.get_output_of_command((travis2tox_path,) + args)
+
+    def get_output_of_command(self, cmd):
+        return subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
+
+    def test_just_script_with_single_command_using_command(self):
+        input = StringIO("""
+language: python
+
+python:
+  - 2.5
+  - 2.6
+  - 2.7
+  - 3.2
+  - pypy
+
+script: python setup.py nosetests
+        """)
+
+        expected_output = b("""
+# Tox (http://tox.testrun.org/) is a tool for running tests
+# in multiple virtualenvs. This configuration file will run the
+# test suite on all supported python versions. To use it, "pip install tox"
+# and then run "tox" from this directory.
+
+[tox]
+envlist = py25, py26, py27, py32, pypy
+
+[testenv]
+commands = python setup.py nosetests
+""")
+
+        self.assertEqual(
+            ## self.get_output_of_command(["travis2tox", "travis.yml"]).strip(),
+            self.travis2tox("travis.yml").strip(),
+            expected_output.strip())
+
